d814444a738870ebfe0f22319efbc7e868ba163c
Daniel Stotts <dstodev@gmail.com>
2020-07-19 23:35:07 -0700

    BoardOptions cleanup

---
+++ mega2560.cmake
@@ -0,0 +1,21 @@
+# Copyright (c) 2020 Arduino CMake Toolchain
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Arduino boards.
+# Platform: Arduino AVR Boards
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+set(ARDUINO_BOARD "Arduino Mega or Mega 2560 [avr.mega]") # Arduino Mega or Mega 2560
+
+#==============================================================================
+# Menu options.
+# Board: Arduino Mega or Mega 2560 [avr.mega]
+#==============================================================================
+
+# Option: Processor
+set(ARDUINO_AVR_MEGA_MENU_CPU_ATMEGA2560 TRUE) # ATmega2560 (Mega 2560)
+
+#******************************************************************************
+# Arduino programmers.
+# Platform: Arduino AVR Boards
+#******************************************************************************
+set(ARDUINO_PROGRAMMER "AVRISP mkII [avr.avrispmkii]") # AVRISP mkII
