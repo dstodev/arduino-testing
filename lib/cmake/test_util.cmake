424ff8362f31b6fdf8fb133e6606e643bceb67a2
Daniel Stotts <dstodev@gmail.com>
2020-07-20 23:18:02 -0700

    Adds 'upload to board' task

--- lib/cmake/test_util.cmake
+++ lib/cmake/test_util.cmake
@@ -3,7 +3,10 @@
 include(GoogleTest)
 
 function(util_add_test name)
-	cmake_parse_arguments(${name} "" "" "FILES;INCLUDES;LIBRARIES" ${ARGN})
+	set(options "")
+	set(oneValueArgs "")
+	set(multiValueArgs FILES INCLUDES LIBRARIES)
+	cmake_parse_arguments(${name} "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
 	if(NOT ${name}_FILES)
 		message(FATAL_ERROR "Missing required parameter: FILES")
@@ -12,19 +15,17 @@ function(util_add_test name)
 	add_executable(${name}
 		${${name}_FILES}
 	)
+
 	target_link_libraries(${name}
 		PRIVATE
-			gtest
 			gtest_main
 			${${name}_LIBRARIES}
 	)
 
-	if(${name}_INCLUDES)
-		target_include_directories(${name}
+	target_include_directories(${name}
 		PRIVATE
 			${${name}_INCLUDES}
-		)
-	endif()
+	)
 
 	gtest_discover_tests(${name})
 endfunction()
--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,16 @@
 const int PIN_LED = 11;
 const int PIN_BUTTON = 2;
 
+const int PIN_LED_BOARD = 13;
+
 void toggleButton()
 {
 	if (digitalRead(PIN_BUTTON) == LOW) {
 		digitalWrite(PIN_LED, HIGH);
+		digitalWrite(PIN_LED_BOARD, HIGH);
 	} else {
 		digitalWrite(PIN_LED, LOW);
+		digitalWrite(PIN_LED_BOARD, LOW);
 	}
 }
 
@@ -19,6 +23,8 @@ void setup()
 	Serial.begin(115200);
 
 	pinMode(PIN_LED, OUTPUT);
+	pinMode(PIN_LED_BOARD, OUTPUT);
+
 	pinMode(PIN_BUTTON, INPUT_PULLUP);
 
 	attachInterrupt(digitalPinToInterrupt(PIN_BUTTON), toggleButton, CHANGE);
